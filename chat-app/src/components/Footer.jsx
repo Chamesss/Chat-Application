77419cd8d2cf3c7e1bbece8b553b1d9957b0052e
Chamesss <chamsedin.azouz@gmail.com>
2023-12-03 20:53:40 +0100

    adjusting

--- chat-app/src/components/Footer.jsx
+++ chat-app/src/components/Footer.jsx
@@ -8,25 +8,25 @@ const Footer = () => {
         window.location.href = `mailto:chamsedin.azouz@gmail.com`;
     }
     return (
-        <Container bgColor={colorMode === 'light' ? '#000032' : '#00001E'} maxW={'97%'} textAlign='center' centerContent p={8} mt={8} mb={5} borderRadius={6} boxShadow='dark-lg'>
+        <Container bgColor={colorMode === 'light' ? '#000032' : '#00001E'} maxW='97%' textAlign='center' centerContent p={8} mt={8} mb={5} borderRadius={6} boxShadow='dark-lg'>
             <VStack>
                 <HStack spacing={6}>
                     <GitSvg /><LinkedinSvg /><WhatsappSvg />
                 </HStack>
-                <Stack display={'flex'} w='120%' justifyContent='space-between' alignItems={'center'} p={2} direction={['column', 'row']} mt={4}>
-                    <HStack display={'flex'} columnGap={2}>
-                        <Phone /> <Text color={'#EEF5FF'}>+216 92448974</Text>
+                <Stack display='flex' w='120%' justifyContent='space-between' alignItems='center' p={2} direction={['column', 'row']} mt={4}>
+                    <HStack display='flex' columnGap={2}>
+                        <Phone /> <Text color='#EEF5FF'>+216 92448974</Text>
                     </HStack>
-                    <HStack display={'flex'} columnGap={2}>
-                        <Email /> <Text sx={{ '&:hover': { cursor: 'pointer' } }} onClick={handleEmailClick} color={'#EEF5FF'}>chamsedin.azouz@gmail.com</Text>
+                    <HStack display='flex' columnGap={2}>
+                        <Email /> <Text sx={{ '&:hover': { cursor: 'pointer' } }} onClick={handleEmailClick} color='#EEF5FF'>chamsedin.azouz@gmail.com</Text>
                     </HStack>
                 </Stack>
             </VStack>
             <Stack display='flex' direction={['column', 'row']} justifyContent='space-around' w='100%' mt={8}>
-                <Text color={'rgba(255,255,255,0.7)'}>
+                <Text color='rgba(255,255,255,0.7)'>
                     Copyright &copy; 2023 All Rights Reserved.
                 </Text>
-                <Text color={'rgba(255,255,255,0.7)'}>
+                <Text color='rgba(255,255,255,0.7)'>
                     Created by <span style={{ color: 'white', fontWeight: '700', opacity: '1' }}>Chamsedin Azouz</span>.
                 </Text>
             </Stack>
