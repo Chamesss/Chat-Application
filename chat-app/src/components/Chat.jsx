070bc977e943a7ac94d18a2241e6ec5d59ac6bc3
Chamesss <chamsedin.azouz@gmail.com>
2023-12-05 14:32:06 +0100

    online-users

--- chat-app/src/components/Chat.jsx
+++ chat-app/src/components/Chat.jsx
@@ -1,9 +1,17 @@
-import { InputGroup, InputLeftElement, Stack, Input, useColorMode, Divider, Box, Text, Avatar, Center } from '@chakra-ui/react'
+import { InputGroup, InputLeftElement, Stack, Input, useColorMode, AvatarBadge, Box, Text, Avatar, Center } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 import OnlineUsers from './OnlineUsers'
 
 const Chat = () => {
     const { colorMode } = useColorMode()
+    const Data = [
+        { name: 'Dan Abrahmov', src: 'https://bit.ly/dan-abramov', stats: 'green.500' },
+        { name: 'Kent Dodds', src: 'https://bit.ly/kent-c-dodds', stats: 'grey' },
+        { name: 'Ryan Florence', src: 'https://bit.ly/ryan-florence', stats: 'grey' },
+        { name: 'Prosper Otemuyiwa', src: 'https://bit.ly/prosper-baba', stats: 'grey' },
+        { name: 'Christian Nwamba', src: 'https://bit.ly/code-beast', stats: 'grey' },
+        { name: 'Segun Adebayo', src: 'https://bit.ly/sage-adebayo', stats: 'grey' }
+    ]
     return (
         <Stack p={4}>
             <InputGroup>
@@ -11,65 +19,38 @@ const Chat = () => {
                     <Search2Icon />
                 </InputLeftElement>
                 <Input
-                    variant='flushed'
+                    variant='filled'
                     placeholder='Search...'
+                    border='none'
+                    borderRadius='50px'
+                    mb='10px'
                     sx={{
                         borderColor: colorMode === 'light' ? 'gray.300' : 'gray.600',
                     }}
                 />
             </InputGroup>
             <OnlineUsers />
-            <Divider opacity={1} />
-
-
-            <Box p={1} w='100%' display='flex' justifyContent='space-between' borderRadius='20px' sx={{
-                ':hover': { backgroundColor: colorMode === 'light' ? '#F0F0F0' : '#2E3959', cursor: 'pointer' }
-            }}>
-                <Center direction='row' p={2}>
-                    <Avatar size='lg' name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
-                    <Stack ml='10px' textAlign='start'>
-                        <Text fontWeight='700' fontSize='md'>Dan Abrahmov</Text>
-                        <Text fontWeight='700' color='#6F7276' fontSize='sm'>This is last message</Text>
-                    </Stack>
-                </Center>
-                <Stack display='flex' alignItems='center' p={2}>
-                    <Text color='#6F7276' fontSize='xs'>20:08</Text>
-                    <Text display='flex' justifyContent='center' alignItems='center' color='white' borderRadius='50px' bgColor='#FA474F' p={0} w='24px' h='24px'>2</Text>
-                </Stack>
-            </Box>
-
-            <Box p={1} w='100%' display='flex' justifyContent='space-between' borderRadius='20px' sx={{
-                ':hover': { backgroundColor: colorMode === 'light' ? '#F0F0F0' : '#2E3959', cursor: 'pointer' }
-            }}>
-                <Center direction='row' p={2}>
-                    <Avatar size='lg' name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
-                    <Stack ml='10px' textAlign='start'>
-                        <Text fontWeight='700' fontSize='md'>Kent Dodds</Text>
-                        <Text fontWeight='700' color='#6F7276' fontSize='sm'>This is another last message</Text>
-                    </Stack>
-                </Center>
-                <Stack display='flex' alignItems='center' p={2}>
-                    <Text color='#6F7276' fontSize='xs'>19:58</Text>
-                    <Text display='flex' justifyContent='center' alignItems='center' color='white' borderRadius='50px' bgColor='#FA474F' p={0} w='24px' h='24px'>1</Text>
-                </Stack>
-            </Box>
-
-            <Box p={1} w='100%' display='flex' justifyContent='space-between' borderRadius='20px' sx={{
-                ':hover': { backgroundColor: colorMode === 'light' ? '#F0F0F0' : '#2E3959', cursor: 'pointer' }
-            }}>
-                <Center direction='row' p={2}>
-                    <Avatar size='lg' name='Ryan Florence' src='https://bit.ly/ryan-florence' />
-                    <Stack ml='10px' textAlign='start'>
-                        <Text fontWeight='500' fontSize='md'>Ryan Florence</Text>
-                        <Text fontWeight='500' color='#6F7276' fontSize='sm'>You: This is last message</Text>
-                    </Stack>
-                </Center>
-                <Stack display='flex' alignItems='center' p={2}>
-                    <Text color='#6F7276' fontSize='xs'>15:23</Text>
-                </Stack>
-            </Box>
-
-
+            <Stack maxH='60vh' overflow='auto'>
+                {Data.map((user) => (
+                    <Box p={1} w='100%' display='flex' justifyContent='space-between' borderRadius='20px' sx={{
+                        '&:hover': { backgroundColor: colorMode === 'light' ? '#F0F0F0' : '#2E3959', cursor: 'pointer' }
+                    }}>
+                        <Center direction='row' p={2}>
+                            <Avatar size='lg' name={user.name} src={user.src}>
+                                <AvatarBadge border='2px solid white' boxSize='0.6em' right='8px' bottom='8px' bg={user.stats} />
+                            </Avatar>
+                            <Stack ml='10px' textAlign='start'>
+                                <Text fontWeight='700' fontSize='md'>Dan Abrahmov</Text>
+                                <Text fontWeight='700' color='#6F7276' fontSize='sm'>This is last message</Text>
+                            </Stack>
+                        </Center>
+                        <Stack display='flex' alignItems='center' p={2}>
+                            <Text color='#6F7276' fontSize='xs'>20:08</Text>
+                            <Text display='flex' justifyContent='center' alignItems='center' color='white' borderRadius='50px' bgColor='#FA474F' p={0} w='24px' h='24px'>2</Text>
+                        </Stack>
+                    </Box>
+                ))}
+            </Stack>
         </Stack>
     )
 }
--- chat-app/src/components/OnlineUsers.jsx
+++ chat-app/src/components/OnlineUsers.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Avatar, Stack, AvatarBadge, useColorMode } from '@chakra-ui/react'
 
 const OnlineUsers = () => {
+  const { colorMode } = useColorMode()
+  const Data = [
+    { name: 'Dan Abrahmov', src: 'https://bit.ly/dan-abramov', stats: 'green.500' },
+    { name: 'Kent Dodds', src: 'https://bit.ly/kent-c-dodds', stats: 'grey' },
+    { name: 'Ryan Florence', src: 'https://bit.ly/ryan-florence', stats: 'grey' },
+    { name: 'Prosper Otemuyiwa', src: 'https://bit.ly/prosper-baba', stats: 'grey' },
+    { name: 'Christian Nwamba', src: 'https://bit.ly/code-beast', stats: 'grey' },
+    { name: 'Segun Adebayo', src: 'https://bit.ly/sage-adebayo', stats: 'grey' }
+  ]
   return (
-    <div>OnlineUsers</div>
+    <Stack w='100%' direction='row' overflow='auto' spacing='0.5rem' p={2}>
+      {Data.map((user, index) => (
+        <Avatar size='lg' key={index} name={user.name} src={user.src} sx={{
+          borderRadius: '5px',
+          border: 'none',
+          p: '4px',
+          transform: 'scale(1.08)',
+          '&:hover': {
+            cursor: 'pointer',
+            backgroundColor: colorMode === 'light' ? '#F0F0F0' : '#2E3959'
+          }
+        }}>
+          <AvatarBadge border='2px solid white' boxSize='0.6em' right='8px' bottom='8px' bg={user.stats} />
+        </Avatar>
+      ))}
+    </Stack>
   )
 }
 
